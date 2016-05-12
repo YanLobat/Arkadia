a20c3dfa0e320bd2be1a613e8385b046b10a1a54
YanLobat <yanlavryushev@gmail.com>
2016-05-12 19:00:45 +0300

    finishing models and change logic

--- appConfig.js
+++ appConfig.js
@@ -1,21 +1,20 @@
-require('app-module-path').addPath(__dirname + '/lib');
+require('app-module-path').addPath(__dirname);
 
 exports.setup = function(runningApp, callback) {
   // Nothing ever comes from "x-powered-by", but a security hole
   runningApp.disable("x-powered-by");
 
   // Choose your favorite view engine(s)
-  runningApp.set('views', __dirname+'/lib/');
   runningApp.set('view engine', 'jade');
 
 
   //---- Mounting well-encapsulated application modules (so-called: "mini-apps")
   //---- See: http://expressjs.com/guide/routing.html and http://vimeo.com/56166857
-  //runningApp.use('/hello', require('hello')); // attach to sub-route
-
   // API endpoint attached to root route:
-  runningApp.use('/', require('index')); // attach to root route
+  runningApp.use('/', require('items')); 
 
+  runningApp.use('/departments', require('departments')); // attach to departments route
+  
   if(typeof callback === 'function') {
     callback(runningApp);
   }
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const server = require('nodebootstrap-server');
 const appConfig = require('./appConfig');
 var app = require('express')();
 var Items = require('./lib/items/models/items.js');
+var Departments = require('./lib/departments/models/departments.js');
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/arkadia');
 
@@ -19,22 +20,68 @@ let rows = parsed.data;
 
 Items.remove({},(err) => {
 	if (err){
-		return;
+		throw new Error('Whoops');
 	}
-	rows.forEach((item, i, rows)=>{
-		let obj_item = {
-			date: item[0],
-			department: item[1],
-			item: item[2],
-			price: item[3],
-			quanity: item[4]
-		};
-		let mongo_item = new Items(obj_item);
-		mongo_item.save((err)=>{
-			if (err){
-				return;
-			}
+	Departments.remove({},(err) => {
+		if (err){
+			throw new Error('Whoops');
+		}
+		rows.forEach((row, i, rows)=>{
+			Items.findOne({name: row[2]}, (err,item) => {
+				if (err){
+					throw new Error('Whoops');
+				}
+				let obj_item = {
+					date: row[0],
+					department: row[1],
+					price: row[3],
+					quanity: row[4]
+				};
+				if (item){
+					item.purchases.push(obj_item);
+					item.total_quanity += parseInt(row[4]);
+				}
+				else{
+					let temp = {
+						name: row[2],
+						purchases: obj_item,
+						total_quanity: row[4]
+					};
+					let mongo_item = new Items(temp);
+					mongo_item.save((err)=>{
+						if (err){
+							return;
+						}
+					});
+				}
+			});
+			Departments.findOne({name: row[1]}, (err,dept) => {
+				if (err){
+					throw new Error('Whoops');
+				}
+				let obj_dept = {
+					date: row[0],
+					item: row[2],
+					price: row[3],
+					quanity: row[4]
+				};
+				if (dept){
+					dept.purchases.push(obj_dept);
+				}
+				else{
+					let temp = {
+						name: row[1],
+						purchases: obj_dept
+					};
+					let mongo_dept = new Departments(temp);
+					mongo_dept.save((err)=>{
+						if (err){
+							return;
+						}
+					});
+				}
+			});
 		});
 	});
 });
-console.log(rows);
+
