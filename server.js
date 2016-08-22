1181531fccf2a4f5197a4224be237df350e4196e
Anna Smirnova <ppomexi@gmail.com>
2016-08-22 05:42:06 +0400

    adding depts

--- server.js
+++ server.js
@@ -17,46 +17,46 @@ app = require('nodebootstrap-htmlapp').setup(app);
 
 server.setup(app, appConfig.setup);
 
-let deptsAdd = (row, dept_names) => {
-	let temp_date = new Date(row[0]);
-	let year = temp_date.getFullYear();
-	let obj_dept = {
-		date: row[0],
-		item: row[2],
-		price: row[3],
-		quanity: row[4],
-		year: year
-	};
-	if (!dept_names.includes(row[1])){
-		dept_names.push(row[1]);
-		let temp = {
-			name: row[1],
-			purchases: []
-		};
-		temp['purchases'] = [];
-		temp['purchases'].push(obj_dept);
- 		let mongo_dept = new Departments(temp);
-		mongo_dept.save((err)=>{
-			if (err){
-				return;
-			}
-		});
-	}
-	else{
-		Departments.findOne({name: row[1]}, (err,dept) => {
-			if (err){
-				throw new Error('Whoops');
-			}
-			dept['purchases'].push(obj_dept);
-			dept.save((err)=>{
-				if (err){
-					console.log(dept);
-					throw new Error(err);
-				}
-			})
-		});
-	}
-};
+// let deptsAdd = (row, dept_names) => {
+// 	let temp_date = new Date(row[0]);
+// 	let year = temp_date.getFullYear();
+// 	let obj_dept = {
+// 		date: row[0],
+// 		item: row[2],
+// 		price: row[3],
+// 		quanity: row[4],
+// 		year: year
+// 	};
+// 	if (!dept_names.includes(row[1])){
+// 		dept_names.push(row[1]);
+// 		let temp = {
+// 			name: row[1],
+// 			purchases: []
+// 		};
+// 		temp['purchases'] = [];
+// 		temp['purchases'].push(obj_dept);
+//  		let mongo_dept = new Departments(temp);
+// 		mongo_dept.save((err)=>{
+// 			if (err){
+// 				return;
+// 			}
+// 		});
+// 	}
+// 	else{
+// 		Departments.findOne({name: row[1]}, (err,dept) => {
+// 			if (err){
+// 				throw new Error('Whoops');
+// 			}
+// 			dept['purchases'].push(obj_dept);
+// 			dept.save((err)=>{
+// 				if (err){
+// 					console.log(dept);
+// 					throw new Error(err);
+// 				}
+// 			})
+// 		});
+// 	}
+// };
 Items.remove({},(err) => {
 	if (err){
 		throw new Error('Whoops');
@@ -75,7 +75,7 @@ Items.remove({},(err) => {
 				this.dept_names = [];
 				this.counter = 0;
 			}
-			itemsFilter(row, item_names, done) {
+			itemsFilter(row, item_names, dept_names, done) {
 				let obj_item = {
 					date: row[0],
 					department: row[1],
@@ -90,64 +90,108 @@ Items.remove({},(err) => {
 						total_quanity: row[4]
 					};
 					temp.purchases.push(obj_item);
-					this.push(temp);
+					this.push({'item':temp});
 					return done();
+					// return this.deptsFilter(row, dept_names, done);
 				}
 				else{		
 					Items.findOne({name: row[2]}, (err,item) => {
 						if (err){
 							return done(err);
+							// return this.deptsFilter(row, dept_names, done);
 						}
 						if (!item){
 							return done();
+							// return this.deptsFilter(row, dept_names, done);
 						}
 						item.purchases.push(obj_item);
 						item.total_quanity += parseInt(row[4]);
-						this.push(item);
+						this.push({'item': item});
 						return done();
+						// return this.deptsFilter(row, dept_names, done);
 					});
 				}
 
 			}
+			deptsFilter(row, dept_names, done) {
+				let temp_date = new Date(row[0]);
+				let year = temp_date.getFullYear();
+				let obj_dept = {
+					date: row[0],
+					item: row[2],
+					price: row[3],
+					quanity: row[4],
+					year: year
+				};
+				if (!dept_names.includes(row[1])){
+					dept_names.push(row[1]);
+					let temp = {
+						name: row[1],
+						purchases: []
+					};
+					temp.purchases.push(obj_dept);
+			 		this.push({'dept': temp});
+			 		return done();
+				}
+				else{
+					Departments.findOne({name: row[1]}, (err,dept) => {
+						if (err){
+							return done(err);
+						}
+						if (!dept){
+							return done();
+						}
+						dept.purchases.push(obj_dept);
+						this.push({'dept': dept});
+						return done();
+					});
+				}
+			}
 			_transform(chunk, _, done) {
-				console.log(this.counter++);
 				let itemArray = chunk.split(",");// split the lines on delimiter
-				this.itemsFilter(itemArray, this.item_names, done);//check will it be a new item 
+				this.itemsFilter(itemArray, this.item_names, this.dept_names, done);//check will it be a new item 
+				// this.deptsFilter(itemArray, this.dept_names, done);//check will it be a new item 
 			}
 		}
 		class Bulker extends stream.Writable {
-			constructor(coll, capacity) {
+			constructor(coll, coll2, capacity) {
 					super({ objectMode: true });
-					this.coll = coll;
 					this.counter = 0;
-					this.bulk = this.coll.initializeOrderedBulkOp();
+					this.items = coll.initializeOrderedBulkOp();
+					this.depts = coll2.initializeOrderedBulkOp();
 					this.capacity = capacity;
 			}
 			insert(done) {
 				this.bulk.execute((err,result) => {
 					if (err) return done(err); 
-					this.bulk = this.coll.initializeOrderedBulkOp();	
-					return done();
+					this.bulk = this.coll.initializeOrderedBulkOp();
+					if (typeof done == 'Function')	
+						return done();
+					else
+						return;
 				});	
 			}
-			_write(item, _, done) {
-				this.bulk.find( { name: item.name } ).upsert().update({'$set': item}); 
+			_write(element, _, done) {
+				// if (element.dept != undefined){
+				// 	this.depts.find( { name: element.name } ).upsert().update({'$set': element}); 
+				// }
+				// if (element.item != undefined) {
+					this.items.find( { name: element.item.name } ).upsert().update({'$set': element.item}); 
+				// }
 				this.counter++;
-				
+				console.log(this.counter);
 				if (this.counter % this.capacity == 0) {
 					this.insert(done);
 				} 
 				return done();
 			}
 			end(item, _, done) {
-				console.log('here we go');
-				this.insert(done);
-				return done();
+				return this.insert(done);
 			}
 
 		}
 		let transformer = new MyTransform();
-		let bulker = new Bulker(Items.collection, 3000);
+		let bulker = new Bulker(Items.collection, Departments.collection, 3000);
 		readableStream
 		.pipe(liner)
 		.pipe(transformer)
@@ -155,7 +199,8 @@ Items.remove({},(err) => {
 		.on('error', function(){
 			console.log('Error while reading file.');
 		})
-		.on('end', function(){
+		.on('end', function(done){
+			// bulker.insert(done);
 			console.log('Read entire file.')
 		});
 		// .pipe(es.split())
